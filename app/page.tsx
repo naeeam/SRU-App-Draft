351efc629dce3182437d4e68abd28b5fd60759bb
naeeam <125646629+naeeam@users.noreply.github.com>
2026-09-19 14:56:47 +0000

    added per-user data isolation

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function App() {
         ))}
       </ul>
       <div>
-        🥳 App successfully hosted. Try creating a new todo.
+        🥳 Hello World
         <br />
         <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
           Review next steps of this tutorial.
